786bb8d420272c4186747a8f63621f68a199e11f
junh0601 <junh0601@naver.com>
2022-07-23 06:50:07 +0900

    ..

--- js/news.js
+++ js/news.js
@@ -1,33 +1,40 @@
-import json from "./news.json" assert{ type: "json" }
-
 const main = document.getElementById("innerMain")
 
 let entireHtml =""
 
-if(json.length===0){
-    entireHtml="<center><h3>기사가 없습니다.</h3></center>"
-}else{
-    json.forEach( (i) => {
-        let html = `
-            <ul><center>
-                <li><img class="news_img" src=${i.img}></li></center>
-                <li><a href=${i.url} target="_blank">
-                    <h3 class="news_title">${i.title}</h3>
-                </a></li>
-                <li><small>${i.date}</small></li>
-            <li>
-        `;
 
-        i.article.forEach((x)=>{
-            const texts = `
-                <p>${x}</p>
-            `
-            html += texts
-        });
-        html += "</li></ul><hr>"
-        entireHtml += html;
-    });
+fetch('https://raw.githubusercontent.com/junh0601/newsScrap/master/news.json')
+  .then((response) => response.json())
+  .then((data) => {
+    
+        if(data.length===0){
+            entireHtml="<center><h3>기사가 없습니다.</h3></center>"
+        }else{
+            data.forEach( (i) => {
+                let html = `
+                    <ul><center>
+                        <li><img class="news_img" src=${i.img}></li></center>
+                        <li><a href=${i.url} target="_blank">
+                            <h3 class="news_title">${i.title}</h3>
+                        </a></li>
+                        <li><small>${i.date}</small></li>
+                    <li>
+                `;
 
-   
-}
-main.innerHTML = entireHtml;
+                i.article.forEach((x)=>{
+                    const texts = `
+                        <p>${x}</p>
+                    `
+                    html += texts
+                });
+                html += "</li></ul><hr>"
+                entireHtml += html;
+            });
+
+
+        }
+        main.innerHTML = entireHtml;
+
+  });
+
+
