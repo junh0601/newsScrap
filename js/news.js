59906c19be37ebd5f02d0153724b9bb2b2743c60
junh0601 <junh0601@naver.com>
2022-07-23 07:57:59 +0900

    ..

--- js/news.js
+++ js/news.js
@@ -1,4 +1,51 @@
+const htmlDate = document.getElementById("date")
+const htmlTime = document.getElementById("time")
 const main = document.getElementById("innerMain")
+
+const city = document.getElementById("city");
+const weather = document.getElementById("weather");
+
+const API_KEY = "d2aab5f5c4c9fe0fce6481b412cea172";
+
+
+//weather
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main + " / " + data.main.temp
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+// date 
+const now = new Date();
+
+const TextDate = `${now.getFullYear()}년 ${now.getMonth()}월 ${now.getDate()}일`
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    htmlTime.innerText = `${hours}:${minutes}:${seconds}`;
+  }
+  
+  getClock();
+  setInterval(getClock, 1000);
+
+htmlDate.innerText = TextDate;
+
+
+
 
 let entireHtml =""
 
@@ -19,7 +66,6 @@ function quoutify (str) {
         return str
     }
 }
-
 
 
 fetch('https://raw.githubusercontent.com/junh0601/newsScrap/master/news.json')
